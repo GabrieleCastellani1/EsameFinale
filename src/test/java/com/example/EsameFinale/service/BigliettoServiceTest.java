50ca2f99f8f4d0b9db3d0e33d3e6144244e008b1
Gabriele <castellanigabriele1@gmail.com>
2023-03-20 15:55:28 +0100

    last commit

--- src/test/java/com/example/EsameFinale/service/BigliettoServiceTest.java
+++ src/test/java/com/example/EsameFinale/service/BigliettoServiceTest.java
@@ -1,4 +1,35 @@
 package com.example.EsameFinale.service;
 
+import com.example.EsameFinale.model.Spettatore;
+import com.example.EsameFinale.repository.BigliettoRepository;
+import com.example.EsameFinale.repository.SpettatoreRepository;
+import com.example.EsameFinale.service.impl.BigliettoServiceImpl;
+import com.example.EsameFinale.service.impl.SpettatoreServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.TestPropertySource;
+
+import java.sql.Date;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:application_test.properties")
 public class BigliettoServiceTest {
+    private BigliettoService bigliettoService;
+
+    @Mock
+    private BigliettoRepository bigliettoRepository;
+
+    @Mock
+    private SalaService salaService;
+
+    @BeforeEach
+    public void init(){
+        bigliettoService = new BigliettoServiceImpl(bigliettoRepository, salaService);
+    }
 }
