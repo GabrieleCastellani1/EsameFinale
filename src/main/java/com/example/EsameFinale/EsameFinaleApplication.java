ccbca6ad3f30a3e748e0dd79f811b9917ac65c1c
Gabriele <castellanigabriele1@gmail.com>
2023-03-20 09:45:18 +0100

    inizializzazione progetto

---
+++ src/main/java/com/example/EsameFinale/EsameFinaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.EsameFinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameFinaleApplication.class, args);
+	}
+
+}
